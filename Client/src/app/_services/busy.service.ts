92e78dbdb92c4fcd874888a562162d66512dc8dc
devastation5600 <devastation5600@gmail.com>
2021-06-15 17:37:04 -0400

    Add loading spinner, edit page, change api calls

---
+++ Client/src/app/_services/busy.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { NgxUiLoaderService, SPINNER } from 'ngx-ui-loader';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BusyService {
+  busyRequstCount = 0;
+
+  constructor(private spinnerService: NgxUiLoaderService) { }
+
+  busy(){
+    this.busyRequstCount++;
+    this.spinnerService.start();
+  }
+
+  idle(){
+    this.busyRequstCount--;
+    if(this.busyRequstCount <=0) {
+      this.busyRequstCount = 0;
+      this.spinnerService.stop();
+    }
+  }
+}
--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,28 +1,43 @@
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {ToastrModule} from 'ngx-toastr';
+
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
+import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
-import { MemberListComponent } from './members/member-list/member-list.component';
-import { MemberDetailsComponent } from './members/member-details/member-details.component';
+
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
-import {ToastrModule} from 'ngx-toastr';
+
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailsComponent } from './members/member-details/member-details.component';
+import { MemberCardComponent } from './members/member-card/member-card.component';
+
 import { SharedModule } from './_modules/shared.module';
-import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
+
 import { ErrorInterceptor } from './_interceptors/error.interceptor';
+import { JwtInterceptor } from './_interceptors/jwt.interceptor';
+
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
-import { MemberCardComponent } from './members/member-card/member-card.component';
-import { JwtInterceptor } from './_interceptors/jwt.interceptor';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
+
+import {TabsModule} from 'ngx-bootstrap/tabs';
+
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+
+import { NgxUiLoaderModule, SPINNER } from "ngx-ui-loader";
+
+import { LoadingInterceptor } from './_interceptors/loading.interceptor';
 
 @NgModule({
   declarations: [
@@ -37,7 +52,11 @@ import { JwtInterceptor } from './_interceptors/jwt.interceptor';
     TestErrorsComponent,
     NotFoundComponent,
     ServerErrorComponent,
-    MemberCardComponent
+    MemberCardComponent,
+    MemberEditComponent
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
   ],
   imports: [
     BrowserModule,
@@ -45,11 +64,13 @@ import { JwtInterceptor } from './_interceptors/jwt.interceptor';
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    SharedModule
+    SharedModule,
+    NgxUiLoaderModule
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi:true},
-    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi:true}
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi:true},
+    {provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi:true}
   ],
   bootstrap: [AppComponent]
 })
